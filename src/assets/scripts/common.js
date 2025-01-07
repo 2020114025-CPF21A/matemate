2240e098b9d618dc89cccf86341ab118ae5baa0e
Nyoung_Ni <71180175+yheun03@users.noreply.github.com>
2025-01-07 23:38:57 +0900

    swiper 도입에 따른 구조 변경 및 common js를 통해 기능 구현

--- src/assets/scripts/common.js
+++ src/assets/scripts/common.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper', {
+        centeredSlides: true,
+        spaceBetween: 30,
+        // loop: true,
 
+        navigation: {
+          nextEl: '.gotoNext',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'progressbar', // 진행도 표시
+        },
+    });
 });
